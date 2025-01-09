696b029655ea2264e05c9997bfb536f503e647dc
MarcoSigno03 <87857643+MarcoSigno03@users.noreply.github.com>
2025-01-09 19:54:29 +0100

    nuovo

--- src/lib.rs
+++ src/lib.rs
@@ -11,21 +11,39 @@ use wg_2024::network::NodeId;
 use wg_2024::packet::Packet;
 
 /// Parsing file  config.toml
-pub fn parse_config(file: &str) -> Config {
-     let file_str = fs::read_to_string(file).expect("error reading config file");
-     println!("Parsing configuration file...");
-     toml::from_str(&file_str).expect("Error occurred during config file parsing")
-}
 
-#[test]
-fn test_initialization() {
-    let config = parse_config("config_file/config.toml");
 
-    match validate_config(&config) {
-        Ok(_) => println!("Config validation passed!"),
-        Err(e) => {
-            println!("Config validation failed: {e:?}");
-            panic!("Validation failed.");
+impl NetworkInitializer {
+    pub fn parse_config(file: &str) -> Config {
+        let file_str = fs::read_to_string(file).expect("error reading config file");
+        println!("Parsing configuration file...");
+        toml::from_str(&file_str).expect("Error occurred during config file parsing")
+    }
+
+    pub fn test_initialization() {
+        let config = parse_config("config_file/config.toml");
+
+        match validate_config(&config) {
+            Ok(_) => println!("Config validation passed!"),
+            Err(e) => {
+                println!("Config validation failed: {e:?}");
+                panic!("Validation failed.");
+            }
         }
     }
+}
+impl Initializer for NetworkInitializer{
+    fn run(&self){
+        let config = parse_config("config_file/config.toml");
+
+        match validate_config(&config) {
+            Ok(_) => println!("Config validation passed!"),
+            Err(e) => {
+                println!("Config validation failed: {e:?}");
+                panic!("Validation failed.");
+            }
+        }
+    }
+
+
 }
