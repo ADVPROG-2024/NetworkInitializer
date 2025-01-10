e423448b32c6a242c91978583fe2b1e7e49426e4
MarcoSigno03 <87857643+MarcoSigno03@users.noreply.github.com>
2025-01-10 19:50:29 +0100

    Connessioni al cargo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use crate::NetworkInitializer;
+use std::fs;
+use wg_2024::config::Config;
+use wg_2024::drone::Drone;
+use dronegowski::Dronegowski;
+use simulationcontroller;
 
 fn main(){
-
+    let drone = Dronegowski::new();
 }
+
