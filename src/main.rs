7b9a02fdf155466f45bc23ab8106c24b116e8ce8
MarcoSigno03 <87857643+MarcoSigno03@users.noreply.github.com>
2025-01-27 16:43:59 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,26 @@
 use std::collections::{HashMap, HashSet};
 use std::{fs, thread};
-use std::fs::File;
 use std::hash::Hash;
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use wg_2024::config::Config;
 use wg_2024::drone::Drone;
 use dronegowski::Dronegowski;
-use dronegowski_utils::hosts::{ClientCommand, ClientEvent};
-use log::LevelFilter;
-use simplelog::{ConfigBuilder, WriteLogger};
+use dronegowski_utils::hosts::{ClientCommand, ClientEvent, ServerCommand, ServerEvent};
 use SimulationController::DronegowskiSimulationController;
 use wg_2024::controller::{DroneCommand, DroneEvent};
 use wg_2024::network::NodeId;
 use wg_2024::packet::Packet;
 use client::DronegowskiClient;
-
-/*struct SimulationController {
-    nodes_channels: HashMap<NodeId, Sender<DroneCommand>>,
-    sim_controller_event_recv: Receiver<DroneEvent>,
-}
-
-impl SimulationController {
-    fn new(
-        nodes_channels: HashMap<NodeId, Sender<DroneCommand>>,
-        sim_controller_event_recv: Receiver<DroneEvent>,
-    ) -> Self {
-        Self {
-            nodes_channels,
-            sim_controller_event_recv
-        }
-    }
-
-    fn crash_all(&mut self) {
-        for (_, sender) in self.nodes_channels.iter() {
-            sender.send(DroneCommand::Crash).unwrap();
-        }
-    }
-}*/
+use dronegowski_utils::functions::simple_log;
+use dronegowski_utils::network::{SimulationControllerNode, SimulationControllerNodeType};
 
 fn main(){
-
-    // Logger di simplelog
-    let log_level = LevelFilter::Info;
-    let _logger = WriteLogger::init(
-        log_level,
-        ConfigBuilder::new().set_thread_level(log_level).build(),
-        File::create("output.log").expect("Could not create log file"),
-    );
+    simple_log();
 
     let config = parse_config("config_file/config.toml");
     parse_node(config);
 }
 
-// config.toml -> Config
 pub fn parse_config(file: &str) -> Config {
     let file_str = fs::read_to_string(file).expect("error reading config file");
     println!("Parsing configuration file...");
@@ -60,22 +28,28 @@ pub fn parse_config(file: &str) -> Config {
 }
 
 fn parse_node(config: Config) {
+    let mut nodi: Vec<SimulationControllerNode> = Vec::new();
+
     let (sc_drone_event_send, sc_drone_event_recv) = unbounded::<DroneEvent>();
     let (sc_client_event_send, sc_client_event_recv) = unbounded::<ClientEvent>();
+    let (sc_server_event_send, sc_server_event_recv) = unbounded::<ServerEvent>();
+
 
     let mut channels: HashMap<NodeId, (Sender<Packet>, Receiver<Packet>)> = HashMap::new();
     let mut sc_drone_channels: HashMap<NodeId, Sender<DroneCommand>> = HashMap::new();
     let mut sc_client_channels: HashMap<NodeId, Sender<ClientCommand>> = HashMap::new();
-
+    let mut sc_server_channels: HashMap<NodeId, Sender<ServerCommand>> = HashMap::new();
 
     for drone in &config.drone {
         let (packet_send, packet_recv) = unbounded();
         channels.insert(drone.id, (packet_send, packet_recv));
     }
+
     for client in &config.client {
         let (packet_send, packet_recv) = unbounded();
         channels.insert(client.id, (packet_send, packet_recv));
     }
+
     for server in &config.server {
         let (packet_send, packet_recv) = unbounded();
         channels.insert(server.id, (packet_send, packet_recv));
@@ -88,14 +62,18 @@ fn parse_node(config: Config) {
         let packet_recv = channels[&drone.id].1.clone(); // Packet Receiver Drone (canale su cui riceve i pacchetti il drone)
         let drone_event_send = sc_drone_event_send.clone(); // Controller Send Drone (canale del SC su cui può inviare gli eventi il drone)
         let mut neighbours:HashMap<NodeId, Sender<Packet>> = HashMap::new(); // Packet Send Drone (canali dei nodi vicini a cui può inviare i pacchetti il drone)
+        let mut neighbours_id = Vec::new();
 
         for neighbour_id in drone.connected_node_ids.clone() {
             let Some(channel_neighbour) = channels.get(&neighbour_id) else { panic!("") };
             neighbours.insert(neighbour_id, channel_neighbour.0.clone());
+            neighbours_id.push(neighbour_id);
         }
 
         let (command_send, command_recv) = unbounded::<DroneCommand>();
-        sc_drone_channels.insert(drone.id, command_send);
+        sc_drone_channels.insert(drone.id, command_send.clone());
+
+        SimulationControllerNode::new(SimulationControllerNodeType::DRONE{ drone_channel: command_send, pdr: drone.pdr }, drone.id, neighbours_id, &mut nodi);
 
         handles.push(thread::spawn(move || {
             let mut drone = Dronegowski::new(drone.id, drone_event_send, command_recv, packet_recv, neighbours, drone.pdr);
@@ -109,14 +87,18 @@ fn parse_node(config: Config) {
         let packet_recv = channels[&client.id].1.clone(); // Packet Receiver Client (canale su cui riceve i pacchetti il client)
         let client_event_send = sc_client_event_send.clone(); // Controller Send Client (canale del SC su cui può inviare gli eventi il client)
         let mut neighbours:HashMap<NodeId, Sender<Packet>> = HashMap::new(); // Packet Send Client (canali dei nodi vicini a cui può inviare i pacchetti il client)
+        let mut neighbours_id = Vec::new();
 
         for neighbour_id in client.connected_drone_ids.clone() {
             let Some(channel_neighbour) = channels.get(&neighbour_id) else { panic!("") };
             neighbours.insert(neighbour_id, channel_neighbour.0.clone());
+            neighbours_id.push(neighbour_id);
         }
 
         let (command_send, command_recv) = unbounded::<ClientCommand>();
-        sc_client_channels.insert(client.id, command_send);
+        sc_client_channels.insert(client.id, command_send.clone());
+
+        SimulationControllerNode::new(SimulationControllerNodeType::CLIENT{ client_channel: command_send}, client.id, neighbours_id, & mut nodi);
 
         handles.push(thread::spawn(move || {
             let mut client = DronegowskiClient::new(client.id, client_event_send, command_recv, packet_recv, neighbours);
@@ -127,36 +109,33 @@ fn parse_node(config: Config) {
 
     // Creazione dei server
     for server in &config.server {
+        let packet_recv = channels[&server.id].1.clone(); // Packet Receiver Server (canale su cui riceve i pacchetti il server)
+        let server_event_send = sc_server_event_send.clone(); // Controller Send Server (canale del SC su cui può inviare gli eventi il server)
+        let mut neighbours:HashMap<NodeId, Sender<Packet>> = HashMap::new(); // Packet Send Server (canali dei nodi vicini a cui può inviare i pacchetti il server)
+        let mut neighbours_id = Vec::new();
 
-        // let mut neighbours:HashMap<NodeId, Sender<Packet>> = HashMap::new();
-        // for neighbour_id in server.connected_drone_ids.clone() {
-        //     let Some(channel_neighbour) = channels.get(&neighbour_id) else { panic!("") };
-        //     neighbours.insert(neighbour_id, channel_neighbour.0.clone());
-        // }
+        for neighbour_id in server.connected_drone_ids.clone() {
+            let Some(channel_neighbour) = channels.get(&neighbour_id) else { panic!("") };
+            neighbours.insert(neighbour_id, channel_neighbour.0.clone());
+            neighbours_id.push(neighbour_id);
+        }
 
-        //let (command_send, command_recv) = unbounded();
-        // sim_command_channels.insert(server.id, command_send);
+        let (command_send, command_recv) = unbounded::<ServerCommand>();
+        sc_server_channels.insert(server.id, command_send.clone());
+
+        SimulationControllerNode::new(SimulationControllerNodeType::SERVER{ server_channel: command_send}, server.id, neighbours_id, & mut nodi);
 
         // handles.push(thread::spawn(move || {
         //      let mut server = Server::new(...);
         //
         //      server.run();
         // }));
-
     }
 
     // Passa la lista di nodi al SimulationController
-    DronegowskiSimulationController::new(config.clone(), sc_drone_channels, sc_client_channels, sc_drone_event_recv, sc_client_event_recv);
-
-
-
-    // let simulation_controller = SimulationController::
-
-    // Prova di crash
-    //simulation_controller.crash_all();
+    DronegowskiSimulationController::new(nodi, sc_drone_channels, sc_client_channels, sc_server_channels, sc_drone_event_recv, sc_client_event_recv, sc_server_event_recv);
 
     while let Some(handle) = handles.pop() {
         handle.join().unwrap();
     }
-
 }
